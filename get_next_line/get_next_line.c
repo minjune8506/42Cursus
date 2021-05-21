21a3a7cc6e1bf80778a6f12ed2e33f55ec7543f1
minjune <minjune8506@gmail.com>
2021-05-21 09:11:32 +0900

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,4 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjune <minjkim2@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/20 14:04:53 by minjune           #+#    #+#             */
+/*   Updated: 2021/05/21 09:10:22 by minjune          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int	check_newline(char *store)
+{
+	while (*store)
+	{
+		if (*store == '\n')
+			return (1);
+		store++;
+	}
+	return (0);
+}
+
 int	get_next_line(int fd, char **line)
 {
+	char	temp[BUFFER_SIZE];
+	char	*store;
 
+	if (read(fd, temp, BUFFER_SIZE) > 0)
+	{
+		while (temp)
+			*store++ = *temp++;
+		if (check_newline(store))
+
+	}
+	printf("line : %s", buf);
+	return (0);
+}
+
+int	main(void)
+{
+	char	**line;
+	int	fd;
+
+	fd = open("./test", O_RDONLY);
+	printf("fd : %d\n", fd);
+	get_next_line(fd, line); 
 }
