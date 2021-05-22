22a95fad8bcccf6c51564443384c51f88c2df20d
minjune <minjune8506@gmail.com>
2021-05-22 15:21:36 +0900

    get_next_line && bonus

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -15,6 +15,10 @@
 
 # include <stdlib.h>
 
+int			check_newline(char *buff);
+int			sep_line(char **line, char **store, int new_line);
+int			remain_content(char **line, char **store, int read_size);
+int			get_next_line(int fd, char **line);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 char		*ft_strjoin(char *s1, char *s2);
 char		*ft_strdup(const char *s1);
