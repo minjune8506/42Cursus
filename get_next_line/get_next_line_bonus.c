744e9e6f29de28c2bb8bc4f1c324e213318f2c73
minjkim2 <minjune8506@gmail.com>
2021-05-26 12:11:17 +0900

    get_next_line bonus

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <unistd.h>
 #define FD_MAX 256
 
