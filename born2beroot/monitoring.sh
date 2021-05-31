30dd9ba009a7c3880e427688bdfd2e095b1d175f
minjune8506 <minjune8506@gmail.com>
2021-05-31 11:44:53 -0400

    Network

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -16,3 +16,12 @@ DISK_TOTAL=8192
 DISK_USE=`df -BM | grep -v Filesystem | awk '{sum+=$3}END{print sum}'`
 DISK_PER=$(echo "$DISK_USE $DISK_TOTAL 100" | awk '{printf ("%d", $1 / $2 * $3)}')
 echo "$DISK_USE/8Gb ($DISK_PER%)"
+printf "#CPU load: \n"
+printf "#Last boot: "
+who -b | awk '{print $3" "$4}'
+printf "#LVM use : \n"
+printf "#Connexions TCP : \n"
+printf "#User log: \n"
+IP_ADDRESS=`hostname -I | tr -d " "`
+MAC_ADDRESS=`ip addr | grep "ether" | awk '{print $2}' | tr -d " "`
+echo "#Network: IP $IP_ADDRESS ($MAC_ADDRESS)"
