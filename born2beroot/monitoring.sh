017de306b990490d773d99cbc07278c8c4aab5ce
Debian <minjkim2@Debian.befkqurq413e5i0rmdgqzalnra.psx.internal.cloudapp.net>
2021-06-01 03:55:22 +0000

    monitoring.sh

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -16,12 +16,19 @@ DISK_TOTAL=8192
 DISK_USE=`df -BM | grep -v Filesystem | awk '{sum+=$3}END{print sum}'`
 DISK_PER=$(echo "$DISK_USE $DISK_TOTAL 100" | awk '{printf ("%d", $1 / $2 * $3)}')
 echo "$DISK_USE/8Gb ($DISK_PER%)"
-printf "#CPU load: \n"
+printf "#CPU load: "
+top -b -n1 | grep "Cpu(s)" | awk '{printf("%.1f%%\n",$2 + $4)}'
 printf "#Last boot: "
 who -b | awk '{print $3" "$4}'
 printf "#LVM use : \n"
-printf "#Connexions TCP : \n"
-printf "#User log: \n"
+printf "#Connexions TCP : "
+CONNECTIONS=`ss -t | grep -v "State" | wc -l`
+echo "$CONNECTIONS ESTABLISHED"
+printf "#User log: "
+top -b -n1 | grep "user" | awk '{print $6}'
 IP_ADDRESS=`hostname -I | tr -d " "`
 MAC_ADDRESS=`ip addr | grep "ether" | awk '{print $2}' | tr -d " "`
 echo "#Network: IP $IP_ADDRESS ($MAC_ADDRESS)"
+printf "#Sudo : "
+SUDO_CMD=`sudo ls -l /var/log/sudo/00/00 | wc -l`
+echo "$SUDO_CMD cmd"
