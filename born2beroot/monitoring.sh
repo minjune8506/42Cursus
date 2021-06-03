ee1f469d7d5a2583f807ec487c2c40f1ad07b63b
minjune <minjune8506@gmail.com>
2021-06-03 06:09:38 -0400

    monitoring.sh

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -12,10 +12,10 @@ MEMORY_USE=`free -m | grep Mem | awk '{print $3}'`
 MEMORY_PERCENTAGE=`free | grep Mem | awk '{printf("%.2f", $3/$2 * 100.0)}'`
 echo "$MEMORY_USE/${MEMORY_TOTAL}MB ($MEMORY_PERCENTAGE%)"
 printf "#Disk Usage: "
-DISK_TOTAL=8192
+DISK_TOTAL=10240
 DISK_USE=`df -BM | grep -v Filesystem | awk '{sum+=$3}END{print sum}'`
 DISK_PER=`echo "$DISK_USE $DISK_TOTAL 100" | awk '{printf ("%d", $1 / $2 * $3)}'`
-echo "$DISK_USE/8Gb ($DISK_PER%)"
+echo "$DISK_USE/10Gb ($DISK_PER%)"
 printf "#CPU load: "
 top -b -n1 | grep "Cpu(s)" | awk '{printf("%.1f%%\n",$2 + $4)}'
 printf "#Last boot: "
