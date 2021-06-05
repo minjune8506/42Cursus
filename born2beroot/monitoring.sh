f6b8be2e77aff84be42ae4961ba827adb46d2be0
minjune <minjune8506@gmail.com>
2021-06-05 01:54:35 -0400

    monitoring.sh disk usage change

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -12,17 +12,18 @@ MEMORY_USE=`free -m | grep Mem | awk '{print $3}'`
 MEMORY_PERCENTAGE=`free | grep Mem | awk '{printf("%.2f", $3/$2 * 100.0)}'`
 echo "$MEMORY_USE/${MEMORY_TOTAL}MB ($MEMORY_PERCENTAGE%)"
 printf "#Disk Usage: "
-DISK_TOTAL=10240
-DISK_USE=`df -BM | grep -v Filesystem | awk '{sum+=$3}END{print sum}'`
+DISK_TOTAL=`lsblk | grep ^sda | awk '{printf("%d"), $4 * 1024}'`
+DISK_TOTAL_GB=`lsblk | grep ^sda | awk '{printf("%dGb", $4)}'`
+DISK_USE=`df --total -BM | tail -n 1 | awk '{print $3}'`
 DISK_PER=`echo "$DISK_USE $DISK_TOTAL 100" | awk '{printf ("%d", $1 / $2 * $3)}'`
-echo "$DISK_USE/10Gb ($DISK_PER%)"
+echo "$DISK_USE/$DISK_TOTAL_GB ($DISK_PER%)"
 printf "#CPU load: "
 top -b -n1 | grep "Cpu(s)" | awk '{printf("%.1f%%\n",$2 + $4)}'
 printf "#Last boot: "
 who -b | awk '{print $3" "$4}'
 printf "#LVM use :"
-LVM_DATA=`lsblk | grep "lvm" | wc -l`
-if [ ${LVM_DATA} == 0 ];
+#LVM_DATA=`lsblk | grep "lvm" | wc -l`
+if [ ${LVM_DATA} ];
 then
 	echo "no"
 else
@@ -34,7 +35,7 @@ echo "$CONNECTIONS ESTABLISHED"
 printf "#User log: "
 top -b -n1 | grep "user" | awk '{print $7}'
 IP_ADDRESS=`hostname -I | tr -d " "`
-MAC_ADDRESS=`ip addr | grep "ether" | awk '{print $2}' | tr -d " "`
+MAC_ADDRESS=`ip addr | grep "ether" | awk '{print $2}'`
 echo "#Network: IP $IP_ADDRESS ($MAC_ADDRESS)"
 printf "#Sudo : "
 SUDO_CMD=`ls -l /var/log/sudo/00/00 | grep -v "total" | wc -l`
