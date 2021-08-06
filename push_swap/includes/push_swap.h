0ab636ca398ed0e6fc07e1957f051f88a7dd8220
minjune <minjune8506@gmail.com>
2021-08-06 12:50:24 +0900

    file divide header change

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -1,5 +1,6 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+#include <stdio.h>
 
 typedef struct s_node
 {
@@ -15,7 +16,18 @@ typedef struct s_stack
 	int		size;
 }t_stack;
 
-void	print_error();
+typedef struct s_cmd
+{
+	int count_ra;
+	int count_rb;
+	int count_pa;
+	int count_pb;
+	int count_origin;
+	int pivot_b;
+	int pivot_s;
+}t_cmd;
+void	print_all(t_stack **stack_a, t_stack **stack_b);
+
 /*
 **stack_init.c
 */
@@ -28,7 +40,6 @@ void	push(t_stack *stack, int data);
 void	push_back(t_stack *stack, int data);
 t_node *pop(t_stack *stack);
 t_node 	*pop_back(t_stack *stack);
-void	print_stack(t_stack *stack);
 void	destroy_stack(t_stack *stack);
 /*
 **sort_utils.c
@@ -59,11 +70,41 @@ void check_overlap(int *arr, int size);
 /*
 **parse.c
 */
-void parse(char **av, t_stack **stack);
+void	parse(char **av, t_stack **stack);
 /*
 **utils.c
 */
 void	print_error();
 void	split_free(char **str);
 long long	ft_atoi_long(const char *str);
+/*
+mid.c
+*/
+int		get_mid(t_stack **stack, int size);
+
+/*
+small_sort.c
+*/
+int	get_max(t_stack **stack, int size);
+int	get_min(t_stack **stack, int size);
+void	three_count_sort(t_stack **stack);
+void	three_sort(t_stack **stack);
+void	five_sort(t_stack **stack_a, t_stack **stack_b);
+/*
+small_count_sort.c
+*/
+int	is_sorted(t_stack **stack, int count);
+int	is_b_sorted(t_stack **stack_a, t_stack **stack_b, int count);
+void	five_count_sort(t_stack **stack_a, t_stack **stack_b);
+/*
+get_pivot.c
+*/
+void	insertion_sort(int *list, int n);
+void	get_pivot(t_stack **stack, int *pivot_b, int *pivot_s, int size);
+/*
+notyet
+*/
+void	a_to_b(t_stack **stack_a, t_stack **stack_b, int count);
+void	b_to_a(t_stack **stack_a, t_stack **stack_b, int count);
+void	two_sort_b(t_stack **stack_a, t_stack **stack_b);
 #endif
