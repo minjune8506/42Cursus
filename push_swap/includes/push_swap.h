c7699cf4c339b24a239438e85813b4c1a2671ee2
minjune <minjune8506@gmail.com>
2021-08-06 15:28:31 +0900

    push_swap

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -1,6 +1,5 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-#include <stdio.h>
 
 typedef struct s_node
 {
@@ -18,93 +17,93 @@ typedef struct s_stack
 
 typedef struct s_cmd
 {
-	int count_ra;
-	int count_rb;
-	int count_pa;
-	int count_pb;
-	int count_origin;
-	int pivot_b;
-	int pivot_s;
+	int	count_ra;
+	int	count_rb;
+	int	count_pa;
+	int	count_pb;
+	int	count_origin;
+	int	pivot_b;
+	int	pivot_s;
 }t_cmd;
-void	print_all(t_stack **stack_a, t_stack **stack_b);
 
+void		print_all(t_stack **stack_a, t_stack **stack_b);
 /*
 **stack_init.c
 */
-t_stack *init_stack();
-t_node *init_node(int data);
+t_stack		*init_stack(void);
+t_cmd		*init_cmd(int count);
+t_node		*init_node(int data);
 /*
 **stack_utils.c
 */
-void	push(t_stack *stack, int data);
-void	push_back(t_stack *stack, int data);
-t_node *pop(t_stack *stack);
-t_node 	*pop_back(t_stack *stack);
-void	destroy_stack(t_stack *stack);
+void		push(t_stack *stack, int data);
+void		push_back(t_stack *stack, int data);
+t_node		*pop(t_stack *stack);
+t_node		*pop_back(t_stack *stack);
+void		destroy_stack(t_stack *stack);
 /*
 **sort_utils.c
 */
-void sa(t_stack **stack_a);
-void sb(t_stack **stack_b);
-void ss(t_stack **stack_a, t_stack **stack_b);
-void pa(t_stack **stack_a, t_stack **stack_b);
-void pb(t_stack **stack_a, t_stack **stack_b);
+void		sa(t_stack **stack_a);
+void		sb(t_stack **stack_b);
+void		ss(t_stack **stack_a, t_stack **stack_b);
+void		pa(t_stack **stack_a, t_stack **stack_b);
+void		pb(t_stack **stack_a, t_stack **stack_b);
 /*
 **sort_utils2.c
 */
-void ra(t_stack **stack_a);
-void rb(t_stack **stack_b);
-void rr(t_stack **stack_a, t_stack **stack_b);
+void		ra(t_stack **stack_a);
+void		rb(t_stack **stack_b);
+void		rr(t_stack **stack_a, t_stack **stack_b);
 /*
 **sort_utils3.c
 */
-void rra(t_stack **stack_a);
-void rrb(t_stack **stack_b);
-void rrr(t_stack **stack_a, t_stack **stack_b);
+void		rra(t_stack **stack_a);
+void		rrb(t_stack **stack_b);
+void		rrr(t_stack **stack_a, t_stack **stack_b);
 /*
 **check.c
 */
-void check_valid(char **av);
-void check_range(char **av);
-void check_overlap(int *arr, int size);
+void		check_valid(char **av);
+void		check_range(char **av);
+void		check_overlap(int *arr, int size);
 /*
 **parse.c
 */
-void	parse(char **av, t_stack **stack);
+void		parse(char **av, t_stack **stack);
 /*
 **utils.c
 */
-void	print_error();
-void	split_free(char **str);
+void		print_error(void);
+void		split_free(char **str);
 long long	ft_atoi_long(const char *str);
-/*
-mid.c
-*/
-int		get_mid(t_stack **stack, int size);
-
 /*
 small_sort.c
 */
-int	get_max(t_stack **stack, int size);
-int	get_min(t_stack **stack, int size);
-void	three_count_sort(t_stack **stack);
-void	three_sort(t_stack **stack);
-void	five_sort(t_stack **stack_a, t_stack **stack_b);
+int			get_max(t_stack **stack, int size);
+int			get_min(t_stack **stack, int size);
+void		three_count_sort(t_stack **stack);
+void		three_sort(t_stack **stack);
 /*
 small_count_sort.c
 */
-int	is_sorted(t_stack **stack, int count);
-int	is_b_sorted(t_stack **stack_a, t_stack **stack_b, int count);
-void	five_count_sort(t_stack **stack_a, t_stack **stack_b);
+int			is_sorted(t_stack **stack, int count);
+int			is_b_sorted(t_stack **stack_a, t_stack **stack_b, int count);
+void		five_count_sort(t_stack **stack_a, t_stack **stack_b);
 /*
 get_pivot.c
 */
-void	insertion_sort(int *list, int n);
-void	get_pivot(t_stack **stack, int *pivot_b, int *pivot_s, int size);
+void		insertion_sort(int *list, int n);
+int			get_mid(t_stack **stack, int size);
 /*
-notyet
+sort_a.c
 */
-void	a_to_b(t_stack **stack_a, t_stack **stack_b, int count);
-void	b_to_a(t_stack **stack_a, t_stack **stack_b, int count);
-void	two_sort_b(t_stack **stack_a, t_stack **stack_b);
+void		rotate_back(t_stack **stack_a, t_stack **stack_b, t_cmd *cmd);
+void		a_to_b(t_stack **stack_a, t_stack **stack_b, int count);
+/*
+sort_b.c
+*/
+void		two_sort_b(t_stack **stack_a, t_stack **stack_b);
+void		b_to_a(t_stack **stack_a, t_stack **stack_b, int count);
+
 #endif
---
+++ push_swap/random.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+v1 = Integer(ARGV[0])
+a = (1..v1).to_a
+shfled = a.shuffle
+puts shfled.join(' ')
