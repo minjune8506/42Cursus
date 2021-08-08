5735de0f3e8f8b52d511fe99e7204092731883de
minjune <minjune8506@gmail.com>
2021-08-08 12:43:14 +0900

    norm check

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjkim2 <minjkim2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/07 23:44:44 by minjkim2          #+#    #+#             */
+/*   Updated: 2021/08/07 23:44:44 by minjkim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
-#include "libft.h"
+#include <stdlib.h>
 
 int	main(int ac, char **av)
 {
@@ -11,9 +23,7 @@ int	main(int ac, char **av)
 	stack_a = init_stack();
 	stack_b = init_stack();
 	parse(av, &stack_a);
-	if (ac == 1)
-		return (0);
-	if (is_sorted(&stack_a, stack_a->size))
+	if ((ac == 1) | is_sorted(&stack_a, stack_a->size))
 	{
 		destroy_stack(stack_a);
 		destroy_stack(stack_b);
