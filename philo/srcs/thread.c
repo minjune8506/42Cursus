cb332dc481faf60fe4e93d21c461a660dab41e8e
Minjune <minjune8506@gmail.com>
2021-12-26 18:55:12 +0900

    Add : ft_usleep 함수 이용한 시간 체크

--- philo/srcs/thread.c
+++ philo/srcs/thread.c
@@ -1,105 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjkim2 <minjkim2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 18:28:30 by minjkim2          #+#    #+#             */
+/*   Updated: 2021/12/26 18:28:30 by minjkim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 #include <unistd.h>
 
-void	*philosopher(void *arg);
-void	*monitor(void *arg);
-void	even_philo(t_philo *philo);
-void	odd_philo(t_philo *philo);
+void	*routine(void *arg);
+void	do_routine(t_philo *philo);
 
-int init_thread(t_state *state)
+int
+	init_thread(t_state *state)
 {
-	pthread_t *th_id;
-	void *retval;
-	int i;
+	int	i;
 
 	i = 0;
-	th_id = (pthread_t *)malloc(sizeof(pthread_t) * state->num_philo);
 	state->start = get_time();
-	state->running = 1;
 	while (i < state->num_philo)
 	{
-		pthread_create(&th_id[i], NULL, philosopher, &state->philos[i]);
+		if (pthread_create(&state->th_id[i], NULL, routine, &state->philos[i]))
+			return (EXIT_FAILURE);
 		i++;
 	}
+	usleep(1000);
+	if (pthread_create(&state->monitor, NULL, monitor, state))
+		return (EXIT_FAILURE);
 	i = 0;
 	while (i < state->num_philo)
 	{
-		pthread_join(th_id[i], &retval);
+		if (pthread_join(state->th_id[i], NULL))
+			return (EXIT_FAILURE);
 		i++;
 	}
+	if (pthread_join(state->monitor, NULL))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-void *philosopher(void *arg)
+void
+	*routine(void *arg)
 {
-	t_philo *philo;
-	pthread_t th_id;
-	void *retval;
+	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	// printf("%lld Philosopher %d is ready\n",(get_time() - philo.state->start), philo.id);
-	philo->last_eat = get_time();
-	philo->deadline = philo->last_eat + philo->state->time_to_die;
-	pthread_create(&th_id, NULL, &monitor, philo);
+	philo->deadline = philo->state->start + philo->state->time_to_die;
+	if (!philo->id % 2)
+		usleep(100);
 	while (1)
 	{
-		if (philo->state->running == 0)
-			break;
-		if (philo->id % 2 == 0)
-			even_philo(philo);
-		else
-			odd_philo(philo);
+		if (!philo->state->running || philo->state->num_philo == 1)
+			break ;
+		do_routine(philo);
 	}
+	return (NULL);
 }
 
-void *monitor(void *arg)
-{
-	t_philo *philo;
-
-	philo = (t_philo *)arg;
-	while (1)
-	{
-		if (!philo->is_eating && get_time() > philo->deadline)
-		{
-			// Philo is dead case
-			print_message(philo, DEAD);
-			philo->state->running = 0;
-			break;
-		}
-		usleep(1000);
-	}
-}
-
-void even_philo(t_philo *philo)
-{
-	grab_fork(philo->state, philo->right_fork);
-	grab_fork(philo->state, philo->left_fork);
-	print_message(philo, FORK);
-	print_message(philo, EAT);
-
-	philo->is_eating = 1;
-	usleep(philo->state->time_to_eat * 1000);
-	philo->last_eat = get_time();
-	philo->deadline = philo->last_eat + philo->state->time_to_die;
-
-	down_fork(philo->state, philo->right_fork, philo->left_fork);
-
-	print_message(philo, SLEEP);
-	usleep(philo->state->time_to_sleep * 1000);
-}
-
-void odd_philo(t_philo *philo)
+void
+	do_routine(t_philo *philo)
 {
 	grab_fork(philo->state, philo->left_fork);
 	grab_fork(philo->state, philo->right_fork);
-
 	print_message(philo, FORK);
 	print_message(philo, EAT);
 	philo->is_eating = 1;
-	usleep(philo->state->time_to_eat * 1000);
 	philo->last_eat = get_time();
 	philo->deadline = philo->last_eat + philo->state->time_to_die;
+	ft_usleep(philo, philo->state->time_to_eat);
 	down_fork(philo->state, philo->right_fork, philo->left_fork);
-
+	philo->is_eating = 0;
+	philo->eat_count++;
 	print_message(philo, SLEEP);
-	usleep(philo->state->time_to_sleep * 1000);
+	ft_usleep(philo, philo->state->time_to_sleep);
+	print_message(philo, THINK);
 }
