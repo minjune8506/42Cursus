6057aeefba02fcb5f8847cdf2486d36a9cd3d18b
Minjune <minjune8506@gmail.com>
2021-12-26 18:54:03 +0900

    Add & Modify : ft_usleep & Norm

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -1,33 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjkim2 <minjkim2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/26 18:28:39 by minjkim2          #+#    #+#             */
+/*   Updated: 2021/12/26 18:28:40 by minjkim2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 #include <sys/time.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void	grab_fork(t_state *state, int c)
+void
+	grab_fork(t_state *state, int c)
 {
 	pthread_mutex_lock(&state->forks_lock[c]);
 }
 
-void	down_fork(t_state *state, int c1, int c2)
+void
+	down_fork(t_state *state, int c1, int c2)
 {
-		pthread_mutex_unlock(&state->forks_lock[c1]);
-		pthread_mutex_unlock(&state->forks_lock[c2]);
+	pthread_mutex_unlock(&state->forks_lock[c1]);
+	pthread_mutex_unlock(&state->forks_lock[c2]);
 }
 
-uint64_t get_time(void)
+uint64_t
+	get_time(void)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-void	print_message(t_philo *philo, int type)
+void
+	print_message(t_philo *philo, int type)
 {
-	if (philo->state->running == 0)
-		return ;
 	pthread_mutex_lock(&philo->state->display_lock);
-	printf(BLUE"%lld\t"RESET, get_time() - philo->state->start);
-	printf("Philosopher %d", philo->id);
+	if (philo->state->running == 0)
+	{
+		pthread_mutex_unlock(&philo->state->display_lock);
+		return ;
+	}
+	printf(BLUE"%lld"RESET, get_time() - philo->state->start);
+	printf(" %d", philo->id);
 	if (type == FORK)
 		printf(" has taken a fork\n");
 	else if (type == EAT)
@@ -37,6 +57,20 @@ void	print_message(t_philo *philo, int type)
 	else if (type == THINK)
 		printf(" is thinking\n");
 	else if (type == DEAD)
-			printf(RED" is dead\n"RESET);
+		printf(RED" died\n"RESET);
 	pthread_mutex_unlock(&philo->state->display_lock);
+}
+
+void
+	ft_usleep(t_philo *philo, int time)
+{
+	unsigned long long	now;
+
+	now = get_time();
+	while (get_time() < now + time)
+	{
+		if (!philo->state->running)
+			break ;
+		usleep(500);
+	}
 }
